d497995014b4fd44e0f19221e4470f51b242bd1f
Gilbert Green <42986583+gibsongreen@users.noreply.github.com>
2021-12-15 21:46:00 -0500

    Add files via upload

---
+++ GroceryList.cpp
@@ -0,0 +1,6 @@
+#include "GroceryList.h"
+
+GroceryList::GroceryList()
+{
+	name = "Grocery List";
+};
--- Refridgerator.cpp
+++ Refridgerator.cpp
@@ -1,36 +1,59 @@
+/** @file Refridgerator.cpp
+
+ *  @brief .cpp file for the Refridgerator class, which inherits from the Container class.
+
+ *
+
+ *  Longer description of file.
+
+ *
+
+ *  @author Gibson Green
+
+ *  @bug No known bugs.
+
+ */
 #include "Refridgerator.h"
 
 Refridgerator::Refridgerator()
 {
-	internal_temperature = 35;
-	name = "Refridgerator";
-}
-int Refridgerator::getInternalTemperature()
+    constexpr int internal_temp = 35;
+    internal_temperature = internal_temp;
+    name = "Refridgerator";
+};
+
+auto Refridgerator::getInternalTemperature() const noexcept -> int
 {
-	return internal_temperature;
-}
-void Refridgerator::setInternalTemperature(int t)
+    return internal_temperature;
+};
+
+void Refridgerator::setInternalTemperature(int t) noexcept
 {
-	internal_temperature = t;
-}
+    internal_temperature = t;
+};
+
 void Refridgerator::changeInternalTemperature()
 {
-	cout << "Current internal temperature of fridge: " << getInternalTemperature() << endl;
-	cout << "Enter desired new internal temperature of fridge: " << endl;
+    cout << "Current internal temperature of fridge: " << getInternalTemperature() << endl;
+    cout << "Enter desired new internal temperature of fridge: " << endl;
 
-    int newTemp;
+    int newTemp = 0;
     try
     {
         cin >> newTemp;
-        if ((newTemp < 32) || (newTemp > 45))
+        constexpr int low = 32;
+        constexpr int high = 45;
+
+        if ((newTemp < low) || (newTemp > high))
         {
             cout << "Temperature range of fridge is 32-45, retry selection" << endl;
             cin >> newTemp;
         }
     }
-    catch (invalid_argument& e) {
+    catch (const invalid_argument& e)
+    {
         cout << e.what() << endl;
     }
     internal_temperature = newTemp;
     cout << "The internal temperature is set to: " << internal_temperature << endl;
-}
+};
